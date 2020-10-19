12397b4ee2843caa3bcbca39feaadbfe008f644f
Niels Lensink <nielslensink@gmail.com>
2020-10-19 16:13:49 +0200

    Added docstring explaining the setting required for this middleware to work

--- api/src/middleware/gzip.py
+++ api/src/middleware/gzip.py
@@ -1,4 +1,9 @@
 class UWSGIGZipMiddleware:
+    """
+    Let uWSGI do the gzip encoding.
+    With the UWSGI http-auto-gzip (UWSGI_HTTP_AUTO_GZIP=1) setting
+    enabled we can instruct it using the uWSGI-Encoding header
+    """
     def __init__(self, get_response):
         self.get_response = get_response
 
