d63c69df42f1c06db6f37f4be538b4bd1e351f06
Niels Lensink <nielslensink@gmail.com>
2021-05-20 12:54:51 +0200

    Added management command to process data in bulk

--- api/src/passage/management/commands/passage_privacy.py
+++ api/src/passage/management/commands/passage_privacy.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from django.core.management.base import BaseCommand
 from django.db.models import Case, F, Value, When
@@ -10,47 +11,49 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--batch-size', nargs='?', default=1000, type=int)
+        parser.add_argument('--sleep', nargs='?', default=1, type=int)
+
     def handle(self, **options):
         verbosity = int(options['verbosity'])
         logger.info("message")
 
-        fields = [
-            'datum_eerste_toelating',
-            'datum_tenaamstelling',
-            'toegestane_maximum_massa_voertuig',
-            'europese_voertuigcategorie_toevoeging',
-            'inrichting',
-            'merk',
-        ]
-        subset = Passage.objects.filter().values('pk')[0:3]
-        qs = Passage.objects.filter(pk__in=subset).update(
-            datum_eerste_toelating=TruncYear('datum_eerste_toelating'),
-            datum_tenaamstelling=Value(None),
-            toegestane_maximum_massa_voertuig=Case(
-                When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(1500)),
-                default=F('toegestane_maximum_massa_voertuig'),
-            ),
-            europese_voertuigcategorie_toevoeging=Case(
-                When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
-                default=F('europese_voertuigcategorie_toevoeging'),
-            ),
-            inrichting=Case(
-                When(voertuig_soort__iexact='PeRsoNenAutO', then=Value('personenauto')),
-                default=F('inrichting'),
-            ),
-            merk=Case(
-                When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
-                default=F('merk'),
-            ),
-        )
-        raise Exception('FAIL')
-        #  for o in qs:
-            #  print('-' * 80)
-            #  for field in fields:
-                #  new_field = f'x_{field}'
-                #  values = (
-                    #  self.style.WARNING(getattr(o, field))
-                    #  + '->'
-                    #  + self.style.SUCCESS(getattr(o, new_field))
-                #  )
-                #  self.stdout.write(f"{field}: {values}")
+        batch_size = options['batch_size']
+        sleep = options['sleep']
+
+        batch = self.get_batch(batch_size)
+        while batch:
+            qs = Passage.objects.filter(pk__in=batch).update(
+                datum_eerste_toelating=TruncYear('datum_eerste_toelating'),
+                datum_tenaamstelling=Value(None),
+                toegestane_maximum_massa_voertuig=Case(
+                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(1500)),
+                    default=F('toegestane_maximum_massa_voertuig'),
+                ),
+                europese_voertuigcategorie_toevoeging=Case(
+                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
+                    default=F('europese_voertuigcategorie_toevoeging'),
+                ),
+                inrichting=Case(
+                    When(
+                        voertuig_soort__iexact='PeRsoNenAutO',
+                        then=Value('Personenauto'),
+                    ),
+                    default=F('inrichting'),
+                ),
+                merk=Case(
+                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
+                    default=F('merk'),
+                ),
+                privacy_check=True
+            )
+            self.stdout.write('Processed: ' + self.style.SUCCESS(qs))
+            self.stdout.write('sleeping for: ' + self.style.SUCCESS(sleep))
+            time.sleep(sleep)
+            batch = self.get_batch(batch_size)
+
+        self.stdout.write(self.style.SUCCESS('Finished'))
+
+    def get_batch(self, size):
+        return Passage.objects.filter(privacy_check=False).values('pk')[0:size]
