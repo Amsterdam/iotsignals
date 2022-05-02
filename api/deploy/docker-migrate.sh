90356dada1d00b5182b8cf2b3a7acea2c772915f
Jeffrey Klardie <j.klardie@amsterdam.nl>
2022-05-02 10:27:36 +0200

    Migrate up to and including passage 0020

--- api/deploy/docker-migrate.sh
+++ api/deploy/docker-migrate.sh
@@ -6,5 +6,9 @@ set -x   # print what we are doing
 
 cd /app/
 
-yes yes | python manage.py migrate --noinput
+# there are two apps, contenttypes (from Django) and passages
+yes yes | python manage.py migrate contenttypes --noinput
+
+# migrate passage up to specific migration during schema migration process
+yes yes | python manage.py migrate passage 0020 --noinput
 
