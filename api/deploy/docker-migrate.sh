e06b59c3d3f8d2aba07e5aabc85129ee7fabd3ac
Jeffrey Klardie <j.klardie@amsterdam.nl>
2022-05-06 13:13:43 +0200

    Removed massa_ledig_voertuig_new (tmp value during migration

--- api/deploy/docker-migrate.sh
+++ api/deploy/docker-migrate.sh
@@ -6,9 +6,5 @@ set -x   # print what we are doing
 
 cd /app/
 
-# there are two apps, contenttypes (from Django) and passages
-yes yes | python manage.py migrate contenttypes --noinput
-
-# migrate passage up to specific migration during schema migration process
-yes yes | python manage.py migrate passage 0022 --noinput
+yes yes | python manage.py migrate --noinput
 
