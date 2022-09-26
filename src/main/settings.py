4dd4de0c6a8b29e0fb5269cbb8cbb17a36cd43bb
Nick Zaaijer <n.zaaijer@amsterdam.nl>
2022-09-26 10:38:18 +0200

    remove duplicate timezone(keep latest imported)

--- src/main/settings.py
+++ src/main/settings.py
@@ -82,9 +82,6 @@ TEMPLATES = [
     },
 ]
 
-
-
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
@@ -164,19 +161,6 @@ TEMPLATES = [
         },
     }
 ]
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
 
 DUMP_DIR = "mks-dump"
 
